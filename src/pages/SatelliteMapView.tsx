fbf54b2f72e1cc4c7ede0ff83ecc215d64fbf84d
Lypho2012 <ioi2020acsl@gmail.com>
2023-07-19 10:56:18 -0700

    add search bar

--- src/pages/SatelliteMapView.tsx
+++ src/pages/SatelliteMapView.tsx
@@ -1,70 +1,71 @@
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 import { useEffect, useState } from "react";
 import * as tf from "@tensorflow/tfjs";
-
-/** 
-   * Load satellite segmentation model 
-  */
-async function loadModel() {
-  return await tf.loadLayersModel("public/models/satellite_segmentation_30/model.json");
-}
-
-/**
- * Recenters map to user location
- */
-function recenter() {
-  // Try HTML5 geolocation.
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      (position: GeolocationPosition) => {
-        const pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-
-        setCenter(pos);
-        setZoom(10);
-      }
-    );
-  } else {
-    // Browser doesn't support Geolocation
-    throw Error("Error: Your browser doesn't support geolocation.");
-  }
-}
-
-/**
- * Processes image to patches before predicting
- */
-async function process_image(image:tf.Tensor) {
-  
-  return image;
-}
-
-/**
- * Uses google maps current window to predict road segmentation and returns overlay
- */
-async function predict() {
-  const model = loadModel();
-  const currentMap = tf.browser.fromPixels(document.querySelector("#static-image") as HTMLImageElement);
-  const prediction = (await model).predict(await process_image(currentMap));
-  return prediction;
-}
-
-/**
- * Opens list of reported congestion events in dashboard
- */
-function openCongestionList() {
-
-}
-
-/**
- * Opens list of reported accidents in dashboard
- */
-function openAccidentsList() {
-  
-}
+import TextField from "@mui/material/TextField";
+import { Route, Routes } from "react-router-dom";
 
 function Map() {
+  /** 
+   * Load satellite segmentation model 
+  */
+  async function loadModel() {
+    return await tf.loadLayersModel("public/models/satellite_segmentation_30/model.json");
+  }
+
+  /**
+   * Recenters map to user location
+   */
+  function recenter() {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          setCenter(pos);
+          setZoom(10);
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      throw Error("Error: Your browser doesn't support geolocation.");
+    }
+  }
+
+  /**
+   * Processes image to patches before predicting
+   */
+  async function process_image(image:tf.Tensor) {
+    
+    return image;
+  }
+
+  /**
+   * Uses google maps current window to predict road segmentation and returns overlay
+   */
+  async function predict() {
+    const model = loadModel();
+    const currentMap = tf.browser.fromPixels(document.querySelector("#static-image") as HTMLImageElement);
+    const prediction = (await model).predict(await process_image(currentMap));
+    return prediction;
+  }
+
+  /**
+   * Opens list of reported congestion events in dashboard
+   */
+  function openCongestionList() {
+
+  }
+
+  /**
+   * Opens list of reported accidents in dashboard
+   */
+  function openAccidentsList() {
+    
+  }
 
   // update road segmentation overlay in real-time
   const [roadSegmentation,setRoadSegmentation] = useState(predict()); 
@@ -100,6 +101,16 @@ function Map() {
           <button onClick={() => openCongestionList()}>Congestion</button>
           <button onClick={() => openAccidentsList()}>Accidents</button>
         </div>
+        <TextField
+          id="satellitemapview_dashboard_searchbar"
+          variant="outlined"
+          fullWidth
+          label="Search"
+        />
+        <Routes>
+          <Route path="/" element={}></Route>
+          <Route path="/drone-management" element={}></Route>
+        </Routes>
       </div>
     </div>
   )
